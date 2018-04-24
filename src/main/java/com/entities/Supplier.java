6369112d9d3a41f29ffa84eb1a8601bed04daecc
Natalie Eitan <natalie.eitan@gmail.com>
2018-04-24 21:05:59 +0300

    comment for example

--- src/main/java/com/entities/Supplier.java
+++ src/main/java/com/entities/Supplier.java
@@ -3,14 +3,14 @@ package com.entities;
 import com.utilities.Area;
 import com.utilities.Cities;
 import com.utilities.Style;
-
+// test
 public class Supplier extends User {
 
     private String vanueName;
     private String phone;
     private String phone2;
     private int maxCapacity;
-    private Boolean isGarden; //גן או אולם
+    private Boolean isGarden;
     private Cities city;
     private Area area;
     private int minPricePerPerson;
