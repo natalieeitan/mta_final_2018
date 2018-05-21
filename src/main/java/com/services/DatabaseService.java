340abaeb52f4d40b67362c02168740b1fa4c1d70
Natalie Eitan <natalie.eitan@gmail.com>
2018-05-21 13:32:20 +0300

    more logic

--- src/main/java/com/services/DatabaseService.java
+++ src/main/java/com/services/DatabaseService.java
@@ -1,8 +1,14 @@
 package com.services;
 
+import com.entities.Couple;
 import com.entities.User;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.utilities.SchedulingRange;
 
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -18,5 +24,15 @@ public class DatabaseService {
 	public long addUser(User user) {
 		usersList.add(user);
 		return user.getId();
+	}
+
+
+	public Couple findCoupleById(int id) {
+		return null;
+	}
+
+	public void updateCouple(Couple couple) {
+		//db.findById()
+		//db.update(couple);
 	}
 }
--- src/main/java/com/services/UserService.java
+++ src/main/java/com/services/UserService.java
@@ -8,7 +8,7 @@ import java.util.List;
  * Created by natalieeitan on 09/04/2018.
  */
 public class UserService {
-	//List<User> usersList = EmployeeList.getInstance();
+//	List<User> usersList = EmployeeList.getInstance();
 
 //	public List<User> getAllEmployees() {
 //		return usersList;
