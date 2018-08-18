806241c65d95af9df24b024d5bcf5f5323dd00fa
Yarden Embar rotbart <yarden.imbar.rotbart@gmail.com>
2018-08-18 19:40:56 +0300

    added form of couples

--- src/main/java/com/utilities/PriceRange.java
+++ src/main/java/com/utilities/PriceRange.java
@@ -2,10 +2,12 @@ package com.utilities;
 
 public enum PriceRange{
     NoRange("NoRange"),
-    One("100-150"),
+    OneFifty("150-200"),
     Two("200-250"),
     Three("300-350"),
+    ThreeFifty("350-400"),
     Four("400-450"),
+    FourFifty("450-500"),
     Five("500 and above")
     ;
     private String name;
