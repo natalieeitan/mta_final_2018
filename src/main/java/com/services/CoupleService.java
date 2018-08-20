cba357675ae8e00e03d28c6f974bae1e88f171bc
Liron <lironfreiman14@gmail.com>
2018-08-21 01:38:38 +0200

    update db get and push functions

--- src/main/java/com/services/CoupleService.java
+++ src/main/java/com/services/CoupleService.java
@@ -6,6 +6,7 @@ import com.utilities.SqlQueries;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class CoupleService {
@@ -16,19 +17,30 @@ public class CoupleService {
         WedAppServer db = new WedAppServer();
 
         String id;
-        String venueName;
-        String phone;
-        int maxCapacity;
-        boolean isGarden;
-        int area;
-        int minPrice;
-        int style;
+        int schedulingRange;
+        Date date;
+        int daysToMarry;
+        int preferredMonths;
+        int areas;
+        int styles;
+        int numOfInvites;
+        int pricing;
 
         try {
-            ResultSet rs = db.getDataFromDB(SqlQueries.GET_ALL_SUPPLIERS);
+            ResultSet rs = db.getDataFromDB(SqlQueries.GET_ALL_COUPLES);
             while (rs.next()) {
                 id = rs.getString("ID");
+                schedulingRange=rs.getInt("SchedulingRange");
+                date=rs.getDate("SpecificDate");
+                daysToMarry=rs.getInt("DaysToMarry");
+                preferredMonths=rs.getInt("PreferredMonths");
+                areas=rs.getInt("Areas");
+                styles=rs.getInt("Styles");
+                numOfInvites=rs.getInt("NumberOfInvites");
+                pricing=rs.getInt("PriceRange");
 
+                Couple couple=new Couple(id,schedulingRange,date,daysToMarry,preferredMonths,areas,styles,numOfInvites,pricing);
+                couplesList.add(couple);
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -37,6 +49,25 @@ public class CoupleService {
         return couplesList;
     }
 
+    public void pushAllCouplesToDB(List<Couple> couples) {
+        WedAppServer db = new WedAppServer();
+        String query;
+        for (Couple couple : couples) {
+            try {
+                if(db.checkIfIDExistInTable("Couple",couple.getID())==1) {
+                    query=SqlQueries.updateCoupleInTable(couple);
+                    db.insertToDB(query);
+                }
+                else {
+                    query=SqlQueries.insertIntoCoupleTable(couple);
+                    db.insertToDB(query);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
+        db.closeConnection();
+    }
 
 }
