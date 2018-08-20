61e1c218dde9fb6b30e7f8446d510c1acc3bb831
Liron <lironfreiman14@gmail.com>
2018-08-20 20:31:04 +0200

    update variable types

--- src/main/java/com/entities/Couple.java
+++ src/main/java/com/entities/Couple.java
@@ -9,36 +9,37 @@ import java.util.Date;
  * just for test
  */
 public class Couple {
-    private String userId;
-    private SchedulingRange schedulingRange;
-    private DayOfWeek daysToMarry;
+    private String ID;
+    private int schedulingRange;
 
     private Date date; //specific Date
-    private Season seasonToMarry; //season to marry
-    private Area area; //areas to marry
-    private Style styles;
+    //private Season seasonToMarry; //season to marry
+    private int daysToMarry; //preferred days to marry
+    private int months;
+    private int areas;  //areas to marry
+    private int styles;  //preferred styles
 
     private int numOfInvites;
     private PriceRange pricing;
 
-    public Couple(String userId, SchedulingRange schedulingRange, DayOfWeek daysToMarry, Date date, Season seasonToMarry,
-            Area area, Style styles, int numOfInvites, PriceRange pricing) {
-        this.userId = userId;
+    public Couple(String userId, int schedulingRange,  Date date,int daysToMarry,
+            int areas, int styles, int numOfInvites, PriceRange pricing) {
+        this.ID = userId;
         this.schedulingRange = schedulingRange;
         this.daysToMarry = daysToMarry;
         this.date = date;
-        this.seasonToMarry = seasonToMarry;
-        this.area = area;
+        //this.seasonToMarry = seasonToMarry;
+        this.areas = areas;
         this.styles = styles;
         this.numOfInvites = numOfInvites;
         this.pricing = pricing;
     }
 
-    public void setSchedulingRange(SchedulingRange schedulingRange) {
+    public void setSchedulingRange(int schedulingRange) {
         this.schedulingRange = schedulingRange;
     }
 
-    public void setDaysToMarry(DayOfWeek daysToMarry) {
+    public void setDaysToMarry(int daysToMarry) {
         this.daysToMarry = daysToMarry;
     }
 
@@ -46,15 +47,15 @@ public class Couple {
         this.date = date;
     }
 
-    public void setSeasonToMarry(Season seasonToMarry) {
-        this.seasonToMarry = seasonToMarry;
+    //public void setSeasonToMarry(Season seasonToMarry) {
+    //    this.seasonToMarry = seasonToMarry;
+    //}
+
+    public void setArea(int areas) {
+        this.areas = areas;
     }
 
-    public void setArea(Area area) {
-        this.area = area;
-    }
-
-    public void setStyles(Style styles) {
+    public void setStyles(int styles) {
         this.styles = styles;
     }
 
@@ -62,8 +63,8 @@ public class Couple {
         this.pricing = pricing;
     }
 
-    public Season getSeasonToMarry(){ return this.seasonToMarry; }
-    public DayOfWeek getDayOfWeek(){ return this.daysToMarry; }
+   // public Season getSeasonToMarry(){ return this.seasonToMarry; }
+    public int getDayOfWeek(){ return this.daysToMarry; }
 
     public PriceRange getPricing() {
         return pricing;
